2a3e926e746dbc1b797a237f4900d2d25d810ff7
alba-contfern <alba.contrerasfernandez@epfl.ch>
2024-04-21 11:28:29 +0200

    first funct

--- project_name/src/filetest.py
+++ project_name/src/filetest.py
@@ -1,1 +1,3 @@
 #python file to test secondary branch
+
+#first code to give the smiles
--- project_name/src/package_code.py
+++ project_name/src/package_code.py
@@ -1,2 +1,22 @@
+from rdkit import Chem
+from PubChemPy import get_cids
+from rdkit.Chem.Draw import IPythonConsole
+IPythonConsole.ipython_useSVG=True
 
- 
+get_cids(c('Aspirin'))
+    
+    
+from rdkit import Chem
+import pubchempy as pcp
+from pubchempy import get_compounds
+
+from rdkit.Chem.Draw import IPythonConsole
+IPythonConsole.ipython_useSVG=True
+
+def get_smiles(compound):
+    c=pcp.get_compounds (compound,'name')
+    d=pcp.get_compounds (compound,'smiles')
+    return c,d
+    
+
+print (get_smiles('aspirin'))
