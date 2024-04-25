6c39404956bc298f0b55b8306ec8509a4a32321d
alba-contfern <alba.contrerasfernandez@epfl.ch>
2024-04-25 16:15:29 +0200

    first function final

--- project_name/src/package_code.py
+++ project_name/src/package_code.py
@@ -1,21 +1,27 @@
 
-
 from rdkit import Chem
 import pubchempy as pcp
 from pubchempy import get_compounds
+from rdkit.Chem import Draw
+import pandas as pd
 
 from rdkit.Chem.Draw import IPythonConsole
 IPythonConsole.ipython_useSVG=True
 
+#function that gets the smile for each molecule
 def get_test(compound):
     results = pcp.get_compounds(compound, 'name')
-    print (results)
     for compound in results:
         smiles= compound.isomeric_smiles
         mol=Chem.MolFromSmiles(smiles)
         return mol
+#function that allows all molecules to be represented at the same time
+list=['glucose','aspirin','formaldehyde']
+def generate_image(list:list):
+    mss=[]
+    for index,value in enumerate(list):
+        mss.append(get_test(value))
+    return Draw.MolsToGridImage(mss)
 
-        
-       
 
-get_test('glucose','aspirin')
+generate_image(list)
